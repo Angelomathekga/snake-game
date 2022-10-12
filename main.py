209fbb9ad7fab8683d961f64ea19dcd3c0705bd6
angelo <angelomathekga@gmail.com>
2022-10-13 00:15:33 +0200

    added the up,down,left,right methods and linked them to the onkey screen inputs

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
+
 snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
 
 
 
