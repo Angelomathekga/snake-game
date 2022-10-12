82f34ad82a1ddeea34fe798407a12d984403b6ad
angelo <angelomathekga@gmail.com>
2022-10-13 01:26:59 +0200

    added scoreboard counter added wall collision and exit loop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,13 +11,14 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+score = ScoreBoard()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.right, "Right")
 screen.onkey(snake.left, "Left")
-
-
 
 # Creates a while loop that refreshes the screen every 0.1 seconds
 game_is_on = True
@@ -24,5 +27,12 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280 or snake.head.ycor() > 280:
+        game_is_on = False
+        score.game_over()
 
 screen.exitonclick()
